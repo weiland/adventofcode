a17fb0af99fc9749f80f67978dabb276201a3e98
Pascal Weiland <weiland@users.noreply.github.com>
2022-12-02 10:39:25 +0100

    Add flake devShell for racket

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Development environment";
+
+  inputs = {
+      nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+          devShell = pkgs.mkShell
+          {
+              buildInputs = [
+                pkgs.racket
+            ];
+          };
+      }
+    );
+}
