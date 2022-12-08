f66a0965ac3def2f1b4cb63f91b813ee6fe2990b
Pascal Weiland <weiland@users.noreply.github.com>
2022-12-08 16:21:08 +0100

    Add livebook Boilerplate

--- commit.sh
+++ commit.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+EXT="livemd"
+
 YEAR=$(date +"%Y")
 PREFIX="https://adventofcode.com/$YEAR/day/"
 DAY=$(date +"%d")
@@ -10,7 +12,7 @@ URL="$PREFIX$DAY_SHORT"
 SESSION=$(cat session.txt)
 echo '⏳'
 HTML=$(curl "$URL" --silent -H "Cookie: session=$SESSION")
-TARGET_FILENAME="./$YEAR/$DAY.rkt"
+TARGET_FILENAME="./$YEAR/$DAY.$EXT"
 
 if [[ $HTML != *"Both parts of this puzzle are complete"* ]]; then
   echo "Did you submit both solutions?"
--- newday.sh
+++ newday.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
+EXT="livemd"
 DAY=$(date +"%d")
 YEAR=$(date +"%Y")
 DAY_SHORT=$(date +"%d" | sed 's/^0*//')
-TARGET_FILENAME="./$YEAR/$DAY.rkt"
+TARGET_FILENAME="./$YEAR/$DAY.$EXT"
 URL="https://adventofcode.com/$YEAR/day/$DAY_SHORT"
 
-sed -e "/define DAY/s/0/$DAY/" boilerplate.rkt > "$TARGET_FILENAME"
+sed -e "/define DAY/s/0/$DAY/" "boilerplate.$EXT" > "$TARGET_FILENAME"
 
 sh download.sh
 
